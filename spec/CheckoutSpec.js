c974bd1cd5e709fb685973e75ee942095fc69147
burlistic <will.webster@mykolab.com>
2014-08-23 22:15:52 +0800

    Corrected prices in tests

--- spec/CheckoutSpec.js
+++ spec/CheckoutSpec.js
@@ -13,54 +13,54 @@ describe("Checkout", function() {
 
 	it("should return the correct running total when one item is added", function() {
 
-	    checkout.addItem(new willSuperStore.Item("apple", 1));
+	    checkout.addItem(new willSuperStore.Item("apple", 0.5));
 
-	    //expect(checkout.total()).toEqual(1);
+	    expect(checkout.total()).toEqual(0.5);
 	});
 
 	it("should return the correct running total when several items are added", function () {
 
-	    checkout.addItem(new willSuperStore.Item("apple", 1));
-	    checkout.addItem(new willSuperStore.Item("apple", 1));
-	    checkout.addItem(new willSuperStore.Item("cherry", 1));
+	    checkout.addItem(new willSuperStore.Item("apple", 0.5));
+	    checkout.addItem(new willSuperStore.Item("apple", 0.5));
+	    checkout.addItem(new willSuperStore.Item("cherry", 5));
 
-	    expect(checkout.total()).toEqual(3);
+	    expect(checkout.total()).toEqual(6);
 	});
 
 	it("should apply the 2 for 3 apples discount and return the correct total", function () {
 
 	    checkout.addDiscount(new willSuperStore.ThreeForTwoAppleDiscount());
 
-	    checkout.addItem(new willSuperStore.Item("apple", 1));
-	    checkout.addItem(new willSuperStore.Item("apple", 1));
-	    checkout.addItem(new willSuperStore.Item("apple", 1));
+	    checkout.addItem(new willSuperStore.Item("apple", 0.5));
+	    checkout.addItem(new willSuperStore.Item("apple", 0.5));
+	    checkout.addItem(new willSuperStore.Item("apple", 0.5));
 
-	    expect(checkout.total()).toEqual(2);
+	    expect(checkout.total()).toEqual(1);
 	});
 
 	it("should apply the 2 for 3 apples discount and return the correct total when 4 apples are purchased", function () {
 
 	    checkout.addDiscount(new willSuperStore.ThreeForTwoAppleDiscount());
 
-	    checkout.addItem(new willSuperStore.Item("apple", 1));
-	    checkout.addItem(new willSuperStore.Item("apple", 1));
-	    checkout.addItem(new willSuperStore.Item("apple", 1));
-	    checkout.addItem(new willSuperStore.Item("apple", 1));
+	    checkout.addItem(new willSuperStore.Item("apple", 0.5));
+	    checkout.addItem(new willSuperStore.Item("apple", 0.5));
+	    checkout.addItem(new willSuperStore.Item("apple", 0.5));
+	    checkout.addItem(new willSuperStore.Item("apple", 0.5));
 
-	    expect(checkout.total()).toEqual(3);
+	    expect(checkout.total()).toEqual(1.5);
 	});
 
 	it("should apply the 2 for 3 apples discount and return the correct total when 4 apples are purchased along with a cherry in the midldle", function () {
 
 	    checkout.addDiscount(new willSuperStore.ThreeForTwoAppleDiscount());
 
-	    checkout.addItem(new willSuperStore.Item("apple", 1));
-	    checkout.addItem(new willSuperStore.Item("apple", 1));
-	    checkout.addItem(new willSuperStore.Item("cherry", 1));
-	    checkout.addItem(new willSuperStore.Item("apple", 1));
-	    checkout.addItem(new willSuperStore.Item("apple", 1));
+	    checkout.addItem(new willSuperStore.Item("apple", 0.5));
+	    checkout.addItem(new willSuperStore.Item("apple", 0.5));
+	    checkout.addItem(new willSuperStore.Item("cherry", 5));
+	    checkout.addItem(new willSuperStore.Item("apple", 0.5));
+	    checkout.addItem(new willSuperStore.Item("apple", 0.5));
 
-	    expect(checkout.total()).toEqual(4);
+	    expect(checkout.total()).toEqual(6.5);
 	});
 
 });
